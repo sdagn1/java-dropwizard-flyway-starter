65d0af94198ff39d43e8ecfe3ca56fd4d61bc8d3
Sam Dagnall <sam.dagnall@kainos.com>
2024-07-31 14:07:46 +0100

    Added script to add Admin/Employee role and added two example users

---
+++ src/main/resources/db/migration/V3__create_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO role (title) VALUES ('Admin');
+INSERT INTO role (title) VALUES ('Employee');
+
+INSERT INTO user (username, password, roleID) VALUES
+    ('sdagn1', 'passwd', 1);
+
+INSERT INTO user (username, password, roleID) VALUES
+    ('schmam', 'schmagnall', 2);
