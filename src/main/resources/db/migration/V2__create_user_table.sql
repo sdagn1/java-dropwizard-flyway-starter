427e414bb51ac10c28080199d0bb032a6fd8fcab
Sam Dagnall <sam.dagnall@kainos.com>
2024-07-31 13:35:49 +0100

    Added script to create the role table

---
+++ src/main/resources/db/migration/V2__create_user_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user (
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    username varchar(20) NOT NULL,
+    password varchar(20) NOT NULL,
+    roleID int,
+    FOREIGN KEY (roleID) REFERENCES role(id)
+);
