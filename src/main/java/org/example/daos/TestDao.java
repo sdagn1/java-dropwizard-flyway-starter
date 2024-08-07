b157bd561e5f4d442828531b886c198c811d1586
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-07 13:56:44 +0100

    Fixed Linter errors

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -13,7 +13,6 @@ public class TestDao {
 
         try (Connection connection = DatabaseConnector.getConnection()) {
             Statement statement = connection.createStatement();
-            
             long start = System.currentTimeMillis();
 
             ResultSet resultSet = statement.executeQuery(
